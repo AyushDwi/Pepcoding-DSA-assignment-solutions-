38ca6bef7d1ec584d121860ac6b4ec256f0bd054
Ayush Dwivedi <91947813+AyushDwi@users.noreply.github.com>
2025-02-27 15:21:53 +0530

    Create 4.Any based to decimal.java

---
+++ Level-1/Functions and Arrays/4.Any based to decimal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner scn=new Scanner (System .in);
+        int n=scn.nextInt();
+        int b=scn.nextInt ();
+        int ans =b_to_d(n,b);
+        System .out.println (ans);
+        
+    }
+
+    public static int b_to_d (int n,int b){
+        int temp=0;
+        int rev=0;
+        int p=0;
+        while (n>0){
+            rev=n%10;
+            temp+=rev*Math.pow(8,p);
+            n=n/10;
+            p++;
+        }
+        return temp ;
+    }
+}
